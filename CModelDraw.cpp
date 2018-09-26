f753018a9d921da5715e77cd015997bf6af35996
prutskov <lehaprutskov@gmail.com>
2018-09-26 23:47:27 +0300

    Added polotno antenns and selector

--- CModelDraw.cpp
+++ CModelDraw.cpp
@@ -26,11 +26,13 @@ void CModelDraw::DrawItem(LPDRAWITEMSTRUCT RECT)
 {
 	Graphics gr(RECT->hDC);
 	Bitmap bmp(RECT->rcItem.right, RECT->rcItem.bottom, &gr);
+	_width = RECT->rcItem.right;
+	_height = RECT->rcItem.bottom;
 	Graphics grBmp(&bmp);
-	grBmp.Clear(Color::Yellow);
+	grBmp.Clear(Color::White);
 
-	Pen penLines(Color::Red, 1);
-	Pen penGreatLines(Color::OrangeRed, 2);
+	Pen penLines(Color::Black, 1);
+	Pen penGreatLines(Color::LightGreen, 2);
 
 	for (int i = 1; i < xmax; i++)
 	{
@@ -51,9 +53,18 @@ void CModelDraw::DrawItem(LPDRAWITEMSTRUCT RECT)
 	{
 		grBmp.DrawLine(&penLines, X(RECT, xmin), Y(RECT, i), X(RECT, xmax), Y(RECT, i));
 	}
-
+	
 	grBmp.DrawLine(&penGreatLines, X(RECT, xmin), Y(RECT, 0), X(RECT, xmax), Y(RECT, 0));
 	grBmp.DrawLine(&penGreatLines, X(RECT, 0), Y(RECT, ymin), X(RECT, 0), Y(RECT, ymax));
+
+	if (!_points.empty())
+	{
+		SolidBrush brushDot(Color::Red);
+		for (size_t i = 0; i < _points.size(); i++)
+		{
+			grBmp.FillEllipse(&brushDot, X(RECT, _points[i].X) - 3.f, Y(RECT, _points[i].Y) - 3.f, 6.f, 6.f);
+		}
+	}
 	
 	gr.DrawImage(&bmp, 0, 0);
 }
@@ -82,4 +93,55 @@ REAL CModelDraw::Height(LPDRAWITEMSTRUCT RECT, float height)
 void CModelDraw::Update()
 {
 	RedrawWindow();
+}BEGIN_MESSAGE_MAP(CModelDraw, CStatic)
+ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+
+void CModelDraw::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	Point pt;
+	double d0 = pixelToX(point.x), d1, doubInt;
+	d1 = modf(d0, &doubInt);
+	if (d1 < 0.5) pt.X = (int)doubInt;
+	else pt.X = (int)++doubInt;
+
+	d0 = pixelToY(point.y);
+	d1 = modf(d0, &doubInt);
+	if (d1 < 0.5) pt.Y = (int)doubInt;
+	else pt.Y = (int)++doubInt;
+
+	if (!_points.empty())
+	{
+		int position;
+		bool check = false;
+		for (size_t i = 0; i < _points.size(); i++)
+		{
+			if (_points[i].X == pt.X && _points[i].Y == pt.Y)
+			{
+				check = true;
+				position = i;
+				break;
+			}
+		}
+		if (!check) _points.push_back(pt);
+		else
+		{
+			_points.erase(_points.begin()+position);
+		}
+	}
+	else _points.push_back(pt);
+	Update();
+	
+	CStatic::OnLButtonDown(nFlags, point);
+}
+
+double CModelDraw::pixelToX(double xPx)
+{
+	return (xmax - xmin) / _width*xPx + xmin;
+}
+
+double CModelDraw::pixelToY(double yPx)
+{
+	return -(ymax - ymin) / _height * yPx + ymax;
 }
--- CModelDraw.h
+++ CModelDraw.h
@@ -11,14 +11,21 @@ class CModelDraw : public CStatic
 
 public:
 	double xmin, xmax, ymin, ymax;
+	int _width, _height;
+	std::vector<Point> _points;
 	CModelDraw();
 	~CModelDraw();
 	virtual void DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct);
+
 	void Update();
 	REAL X(LPDRAWITEMSTRUCT RECT, float x);
 	REAL Y(LPDRAWITEMSTRUCT RECT, float y);
 	REAL Width(LPDRAWITEMSTRUCT RECT, float width);
 	REAL Height(LPDRAWITEMSTRUCT RECT, float height);
+	double pixelToX(double xPx);
+	double pixelToY(double yPx);
 
+	DECLARE_MESSAGE_MAP()
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
 };
 
